b1764fb88f12773d5ebf6e6d59646d0a292139c4
harriet oughton <harrietoughton@gmail.com>
2024-09-14 22:06:02 +0100

    Update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << ENV['DEVELOPMENT_HOSTS']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
